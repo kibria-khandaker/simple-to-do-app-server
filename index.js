afd349ef6e58aaa4b919d4c380d87d15be2f8d5b
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-05-19 03:56:32 +0600

    create server for to do list

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// Mongo Connect set user/pass using .env 
+const uri = "mongodb+srv://simpleToDoList:<password>@cluster0.bllil.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+// main function working for server 
+async function run() {
+    try {
+        //--------
+
+        //--------
+    } finally { }
+}
+run().catch(console.dir)
+
+
+
+
+
+
+
+
+// for get main routing and testing 
+app.get('/', (req, res) => {
+    res.send('To Do List Server is Running')
+});
+
+app.listen(port, () => {
+    console.log('To Do Server Listening to port', port);
+})
