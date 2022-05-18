0e31cafa4781d61aa3d771e24abba210a7a77525
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-05-19 05:36:14 +0600

    Add get and Delete API done

--- index.js
+++ index.js
@@ -8,33 +8,43 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// // Mongo Connect set user/pass using .env 
-// const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bllil.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// Mongo Connect set user/pass using .env 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bllil.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// client.connect(err => {
-//     const collection = client.db("test").collection("devices");
-//     // perform actions on the collection object
-//     client.close();
-// });
+// main function working for server 
+async function run() {
+    try {
+        
+        //--------
+        await client.connect();
+        const tasksCollection = client.db("toDoTaskData").collection("tasks");
 
+        // Get All Data From Database or MDB
+        app.get('/task', async (req, res) => {
+            const query = {};
+            const cursor = tasksCollection.find(query);
+            const tasks = await cursor.toArray();
+            res.send(tasks)
+        })
+        // Data in Database MDB
+        app.post('/task',  async (req, res) => {
+            const task = req.body;
+            const result = await tasksCollection.insertOne(task)
+            res.send(result);
+        })
+        // DELETE items from database & UI
+        app.delete('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await tasksCollection.deleteOne(query);
+            res.send(result);
+        })
+        //--------
 
-// // main function working for server 
-// async function run() {
-//     try {
-//         //--------
-
-//         //--------
-//     } finally { }
-// }
-// run().catch(console.dir)
-
-
-
-
-
-
-
+    } finally { }
+}
+run().catch(console.dir)
 
 // for get main routing and testing 
 app.get('/', (req, res) => {
