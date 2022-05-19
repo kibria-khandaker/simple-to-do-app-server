f3014ae07aec3259a918b8d4a263abffc9f2bbe3
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-05-19 06:10:40 +0600

    Edite MongoDB Object-Id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 // middleware
@@ -15,7 +15,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 // main function working for server 
 async function run() {
     try {
-        
+
         //--------
         await client.connect();
         const tasksCollection = client.db("toDoTaskData").collection("tasks");
